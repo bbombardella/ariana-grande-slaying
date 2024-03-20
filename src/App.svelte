4163d2e604a29f422427b334d1a09cfb5e6ecc3a
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-03-20 14:06:56 +0100

    feat: add ariana

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,18 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import Ariana from "./lib/Ariana.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <h1>Slay, Ariana 💅</h1>
+    <Ariana/>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    main {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+    }
 </style>
---
+++ src/lib/Ariana.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import arianaImage from '../assets/ariana.webp'
+</script>
+
+<img src={arianaImage} class="ariana" alt="Ariana Grande"/>
+
+<style>
+    .ariana {
+        flex: 1;
+        max-width: 100%;
+        aspect-ratio: 6 / 1;
+        margin: 0 1em;
+        object-fit: contain;
+        object-position: bottom;
+        will-change: filter;
+        transition: filter 300ms;
+    }
+
+    .ariana:hover {
+        filter: drop-shadow(0 0 2em #ff3e00aa);
+    }
+</style>
